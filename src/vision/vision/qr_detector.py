3add1b91aded0c3a6ce945761d12281308ac3fd3
Aditeya J Frankur <aditeyajfrankur@gmail.com>
2025-12-11 21:18:27 +0530

    Handled edge cases and added logging

--- src/vision/vision/qr_detector.py
+++ src/vision/vision/qr_detector.py
@@ -2,6 +2,7 @@
 import rclpy
 from rclpy.node import Node
 import cv2
+import os
 import numpy as np
 from pyzbar.pyzbar import decode
 from interfaces.msg import TargetDeviation
@@ -10,11 +11,9 @@ class QRDetector(Node):
     def __init__(self):
         super().__init__('qr_detector')
         self.publisher_ = self.create_publisher(TargetDeviation, '/vision/target_deviation', 1)
-        self.cap = cv2.VideoCapture(11)
-
-        if not self.cap.isOpened():
-            self.get_logger().info('ERROR: Failed to access video feed. Check GStreamer splitter.')
-            return
+        
+        self.cap = None
+        self.device_path = '/dev/video11'
 
         self.timer = self.create_timer(0.033, self.timer_callback)
         self.get_logger().info('QR Detector Node Started')
@@ -28,8 +27,23 @@ class QRDetector(Node):
         self.publisher_.publish(msg)
 
     def timer_callback(self):
+        if not os.path.exists(self.device_path):
+            self.get_logger().warn(f"Waiting for {self.device_path}...", throttle_duration_sec=1.0)
+            if self.cap is not None:
+                self.cap.release()
+                self.cap = None
+            return
+
+        if self.cap is None:
+            self.cap = cv2.VideoCapture(self.device_path, cv2.CAP_V4L2)
+            if not self.cap.isOpened():
+                return
+
         ret, frame = self.cap.read()
         if not ret:
+            self.get_logger().warn(f"Video Feed lost. Waiting...", throttle_duration_sec=1.0)
+            self.cap.release()
+            self.cap = None
             return
 
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -42,7 +56,7 @@ class QRDetector(Node):
             msg.detected = True
             msg.qr_data = qr.data.decode("utf-8")
             
-            self.get_logger().info(f'QR Code: {msg.qr_data}')
+            self.get_logger().info(f'QR Code: {msg.qr_data}', throttle_duration_sec=0.25)
              
 
         else:
@@ -50,11 +64,12 @@ class QRDetector(Node):
             msg.x_error = 0.0
             msg.y_error = 0.0
             msg.qr_data = ""
+            self.get_logger().info(f"QR not detected. Retrying...", throttle_duration_sec=0.5)
 
         self.publisher_.publish(msg)
 
     def destroy_node(self):
-        if self.cap.isOpened():
+        if self.cap is not None and self.cap.isOpened():
             self.cap.release()
         super().destroy_node()
 
